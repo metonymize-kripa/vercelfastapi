af36027a9c978d466bf4fe1368268261c8fbfc17
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-27 13:08:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,26 +4,6 @@ import datetime
 app = FastAPI()
 wsb_dictionary={"SPY":0.1}
 wise_dictionary={"SPY":0.2}
-
-def wsb_parser(symbol):
-    try:
-        return wsb_dictionary[symbol]
-    except
-        return 0
-
-def wise_parser(symbol):
-    try:
-        return wise_dictionary[symbol]
-    except
-        return 0
-    
-skills_dictionary={"WSB":wsb_parser, "WISE":wise_parser}
-
-def dispatch_skill_parser(skill, symbol):
-    try:
-        return skills_dictionary[skill](symbol)
-    except:
-        return 0
     
 @app.get("/parse/{parameter}")
 def parse(parameter: str):

d4731d85887732a63512361466302d5046e70132
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-27 13:02:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ app = FastAPI()
 wsb_dictionary={"SPY":0.1}
 wise_dictionary={"SPY":0.2}
 
-def dispatch_skill_parser(skill, symbol):
-    try:
-        return skills_dictionary[skill](symbol)
-    except:
-        return 0
-
 def wsb_parser(symbol):
     try:
         return wsb_dictionary[symbol]
@@ -22,17 +16,25 @@ def wise_parser(symbol):
         return wise_dictionary[symbol]
     except
         return 0
-
+    
 skills_dictionary={"WSB":wsb_parser, "WISE":wise_parser}
 
+def dispatch_skill_parser(skill, symbol):
+    try:
+        return skills_dictionary[skill](symbol)
+    except:
+        return 0
+
 @app.get("/initialize/{key}")
-def init_parsers(key: str):
-    if key = "TheAnswerIs42":
+def initialize(key: str):
+    if key == "TheAnswerIs42":
         wsb_dictionary={"SPY":0.1}
         wise_dictionary={"SPY":0.2}
+        return {"initialize":"success"}
     else
         wsb_dictionary={}
         wise_dictionary={}
+        return {"initialize":"fail"}
 
 @app.get("/parse/{parameter}")
 def parse(parameter: str):

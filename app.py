1c46640bae5a3093c024b83bdcff840d297a8f9f
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-27 13:07:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -30,32 +30,11 @@ def parse(parameter: str):
     parsed_parameter_list = parameter.strip().split()
     num_parameters_parsed = len(parsed_parameter_list)
     
-    if  num_parameters_parsed == 2:
-        parsed_symbol = parsed_parameter_list[0].upper()
-        parsed_skill = parsed_parameter_list[1].upper()
-        skill_returned_value = dispatch_skill_parser(parsed_skill, parsed_symbol)
-    else:
-        return {"symbol": "no symbol",
-                "skill": "no skill",
-                "skill_output": "no output",
-                "datetime": "no time"}
-        
-    try:
-        return {"symbol": parsed_symbol,
-                "skill": parsed_skill,
-                "skill_output": skill_returned_value,
-                "datetime": datetime.datetime.now().time()}
-    except:
-        return {"symbol": "no symbol",
-                "skill": "no skill",
-                "skill_output": "no output",
-                "datetime": "no time"}
-    """
     return {
         "parameter": parameter,
         "datetime": datetime.datetime.now().time()
     }
-    """
+
 
 @app.get("/")
 def main():

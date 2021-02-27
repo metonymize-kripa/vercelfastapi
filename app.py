89c9724f17f2b24c8026cc57c153e012f4f99fae
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-27 13:10:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 import datetime
 
 app = FastAPI()
-wsb_dictionary={"SPY":0.1}
-wise_dictionary={"SPY":0.2}
     
 @app.get("/parse/{parameter}")
 def parse(parameter: str):

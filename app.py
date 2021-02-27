35edc784539e1e99e1a30a13edc349938408c598
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-02-27 11:17:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,24 @@ app = FastAPI()
 
 @app.get("/example/{parameter}")
 def example(parameter: str):
+    parsed_parameter_list = parameter.strip().split()
+    parsed_symbol = parsed_parameter_list[0].upper()
+    parsed_skill = parsed_parameter_list[1].upper()
+    
+    try:
+        return {"symbol": parsed_symbol,
+                "skill": parsed_skill,
+                "datetime": datetime.datetime.now().time()}
+    except:
+        return {"symbol": "no symbol",
+                "skill": "no skill",
+                "datetime": "no time"}
+    """
     return {
         "parameter": parameter,
         "datetime": datetime.datetime.now().time()
     }
+    """
 
 @app.get("/")
 def main():

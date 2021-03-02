ba7870bef5812bf88b43aad82c74fedd85ca66d3
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-03-02 14:59:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def dispatch_skill_parser(skill, symbol):
     
 @app.get("/parse/{parameter}")
 def parse(parameter: str):
-    parsed_parameter_list = parameter.strip().split()
+    parsed_parameter_list = parameter.strip().split(" ")
     num_parameters_parsed = len(parsed_parameter_list)
     return {"input": parsed_parameter_list}
 """
